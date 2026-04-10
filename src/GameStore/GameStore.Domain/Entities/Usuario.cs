43178ffaf8e2a4046939d0363fb90f5f10f9c13b
Ronnam de Lima da Silva <eronnam@gmail.com>
2026-04-10 20:31:06 -0300

    test: recria testes de usuario com a infraestrutura correta da solucao

---
+++ src/GameStore/GameStore.Domain/Entities/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GameStore.Domain.Entities
+{
+    public class Usuario
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public string Senha { get; private set; }
+
+        public Usuario(string nome, string email, string cpf, string senha)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Senha = senha;
+        }
+
+        public void Validar()
+        {
+            throw new NotImplementedException("As regras de validação de cadastro precisam ser implementadas!");
+        }
+    }
+}
