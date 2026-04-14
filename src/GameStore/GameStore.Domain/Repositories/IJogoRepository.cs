4d62e123b23721373115d967f67f3fe44798bdde
Ronnam de Lima da Silva <eronnam@gmail.com>
2026-04-13 22:05:45 -0300

    test: adiciona testes unitarios de dominio e servico para o cadastro de jogo

---
+++ src/GameStore/GameStore.Domain/Repositories/IJogoRepository.cs
@@ -0,0 +1,10 @@
+﻿using GameStore.Domain.Entities;
+
+namespace GameStore.Domain.Repositories
+{
+    public interface IJogoRepository
+    {
+        bool ExistePorNome(string nome);
+        void Adicionar(Jogo jogo);
+    }
+}
---
+++ src/GameStore/GameStore.UnitTests/Services/CadastroJogoServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using GameStore.Domain.Entities;
+using GameStore.Domain.Repositories;
+using GameStore.Domain.Services;
+using Xunit;
+
+namespace GameStore.UnitTests.Services
+{
+    public class CadastroJogoServiceTests
+    {
+        [Fact]
+        public void Nao_Deve_Criar_Jogo_Quando_Ja_Existe_Na_Base_De_Dados()
+        {
+            // Arrange
+            var nomeExistente = "C# for Beginners";
+            var mockRepository = new Mock<IJogoRepository>();
+
+            mockRepository.Setup(r => r.ExistePorNome(nomeExistente)).Returns(true);
+
+            var service = new CadastroJogoService(mockRepository.Object);
+            var novoJogo = new Jogo(nomeExistente, "Descricao", "Developer");
+
+            // Act & Assert
+            var excecao = Assert.Throws<InvalidOperationException>(() => service.Executar(novoJogo));
+            Assert.Equal("Já existe um jogo com esse nome.", excecao.Message);
+        }
+    }
+}
