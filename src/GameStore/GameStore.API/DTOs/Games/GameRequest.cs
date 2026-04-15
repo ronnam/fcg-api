0c4c022a5b2a5a67681b9de6a4bf6e4650d5401e
hguizzo <heloa.guizzo@capgemini.com>
2026-04-15 19:32:59 -0300

    Adding registration game by admin

---
+++ src/GameStore/GameStore.API/DTOs/Games/GameRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace GameStore.API.DTOs.Games;
+public class GameRequest    
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ src/GameStore/GameStore.API/DTOs/Games/GameResponse.cs
@@ -0,0 +1,20 @@
+﻿using GameStore.Domain.Entities;
+
+namespace GameStore.API.DTOs.Games;
+
+public class GameResponse
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+
+    public static GameResponse FromEntity(Game game)
+    {
+        return new GameResponse
+        {
+            Id = game.Id,
+            Title = game.Title,
+            Price = game.Price
+        };
+    }
+}
