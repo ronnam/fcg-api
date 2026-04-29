98c45980088baffb4992ee470ff7eb65a3cc2c87
hguizzo <heloa.guizzo@capgemini.com>
2026-04-29 16:13:42 -0300

    Removing Game Scope

--- src/GameStore/GameStore.API/DTOs/Games/GameRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GameStore.API.DTOs.Games;
-public class GameRequest    
-{
-    public Guid Id { get; set; }
-    public required string Title { get; set; }
-    public required string Category { get; set; }
-}
